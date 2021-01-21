0b9c0ebce860f7eebfed6f35d41268cfc9e574ec
tiago-gallo <69221777+tiago-gallo@users.noreply.github.com>
2021-01-20 21:59:43 -0300

    atualizacao main.cpp

---
+++ sistema_irrigacao/include/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+    #define _MAIN_H_
+
+    #include "rtos.h"
+    #include "sensor.h"
+    #include "display.h"
+    #include "saida.h"
+
+#endif //_MAIN_H_
--- sistema_irrigacao/src/main.cpp
+++ sistema_irrigacao/src/main.cpp
@@ -1,7 +1,18 @@
-#include <Arduino.h>
+/*
+Trabalho final - Sistema-de-Irrigacao-Automatico
+
+Autores:      Tiago Gallo Faria - RA200014441
+              Danillo Lins      - RA200013897
+Professor:    Fabio Souza
+Disciplina:   Sistemas Operacionais e RTOS de Sistemas Embarcados Instituição
+Instituição:  Centro Universitario Salesiano de São Paulo
+*/
+
+
+#include "main.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  displayInit();  //inicia display
 }
 
 void loop() {
