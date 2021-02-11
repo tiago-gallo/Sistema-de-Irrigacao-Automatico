477de8c3d72bff4de5311d70cf8101720ce435e8
tiago-gallo <69221777+tiago-gallo@users.noreply.github.com>
2021-02-11 16:11:44 -0300

    RTOS

--- sistema_irrigacao/include/rtos.h
+++ sistema_irrigacao/include/rtos.h
@@ -1,1 +1,18 @@
+#ifndef _RTOS_H_
+#define _RTOS_H_
 
+#include "mqtt.h"
+#include "sensor.h"
+#include "display.h"
+#include "saida.h"
+
+#include <Arduino.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+
+void rtosInit();
+
+#endif
